04d00c24a5d97521499e70b9377ea0cb95afc3be
GuILS00 <guilhermelapa107@gmail.com>
2025-07-13 21:38:35 -0300

    Delete src directory and recreate

--- src/tests/test_app.py
+++
@@ -1,8 +0,0 @@
-from src import app
-
-def test_create_task():
-    app.tasks = []  # limpa as tarefas
-    task = app.create_task("Teste", "alta")
-    assert task["title"] == "Teste"
-    assert task["priority"] == "alta"
-    assert task["id"] == 1
